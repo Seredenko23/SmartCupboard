6661cb25f9defc4236da89bdc44bdfd72fc0ad47
unknown <srvladyk555@gmail.com>
2019-05-23 14:51:59 +0300

    Деплой

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/smart-cupboard-Angular'));
+app.use(express.static('./dist/smart-cupboard'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname, 'build', 'index.html'));
+res.sendFile(path.join(__dirname,'/dist/smart-cupboard/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
